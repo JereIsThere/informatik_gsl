acdb91a19a7983045e1abf2a552c8fa95d87b6fd
JereIsThere <46347376+JereIsThere@users.noreply.github.com>
2020-05-03 21:57:37 +0200

    improved sketch

--- arduino_sketch/arduino_sketch.ino
+++ arduino_sketch/arduino_sketch.ino
@@ -1,3 +1,13 @@
+/*
+  Sensoren (readings): *_r
+  Kontrollelemente (control): *_c
+  
+  Lufttemperatur
+  
+  licht dimmbar
+  
+*/
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -7,4 +17,19 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+void setPinModes() {
+  pinMode(temp_r, INPUT);
+  pinMode(temp_c, OUTPUT);
+
+  pinMode(humid_r, INPUT);
+  pinMode(humid_c, OUTPUT);
+
+  //TODO bodenfeuchtigkeit
+
+  pinMode(water_r, INPUT);
+  pinMode(water_c, OUTPUT);
+
+  pinMode(light_c, OUTPUT);
 }
